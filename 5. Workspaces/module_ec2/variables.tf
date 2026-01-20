7e7d0be1c2bce8ddb21e7983b3bad395e3f0d422
Madhuri_Chavan_12 <madhurichavan012@gmail.com>
2026-01-20 12:41:02 +0530

    Create variables.tf

---
+++ 5. Workspaces/module_ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "os_type" {
+  type = string
+  description = "linux | ubuntu | windows"
+}
+
+variable "name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
